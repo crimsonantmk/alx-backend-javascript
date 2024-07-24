ae496a6a90fb2c18c0ef0907dc9a90d8f8d03277
crimsonantmk <crimsonant69@gmail.com>
2024-07-24 13:10:19 -0700

    mainss

---
+++ 0x01-ES6_promise/3-main.js
@@ -0,0 +1,9 @@
+bob@dylan:~$ cat 3-main.js
+import handleProfileSignup from "./3-all";
+
+handleProfileSignup();
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 3-main.js 
+photo-profile-1 Guillaume Salva
+bob@dylan:~$ 
